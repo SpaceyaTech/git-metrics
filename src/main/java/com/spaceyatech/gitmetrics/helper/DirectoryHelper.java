74f4e9e4383d1fcf34847d11066e1badb7f25849
David <davidongora@gmail.com>
2023-07-12 10:58:29 +0300

    fix:remove the prompt for entering file and repository name

--- src/main/java/com/spaceyatech/gitmetrics/helper/DirectoryHelper.java
+++ src/main/java/com/spaceyatech/gitmetrics/helper/DirectoryHelper.java
@@ -6,31 +6,33 @@ import java.util.Scanner;
 public class DirectoryHelper {
     public static void main(String[] args) {
         String homeDirectory = System.getProperty("user.home");
+    		//String homeDirectory = "/Users/mac/Desktop";
 
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter the file name: ");
-        String fileName = scanner.nextLine();
-
-        System.out.print("Enter the repository name for the cloned folder: ");
-        String repositoryName = scanner.nextLine();
-
-        String directoryPath = homeDirectory + File.separator + repositoryName;
+        String directoryPath = homeDirectory + File.separator + "/repositoryName";
 
         File directory = new File(directoryPath);
 
         if (directory.exists()) {
-            System.out.println("Directory already exists.");
-        } else {
-            boolean created = directory.mkdir();
+        	if (deleteDirectory(directory)) {
+        		System.out.println("Deleted the existing directory" + directory);
+        	} else {
+        		System.err.println("Failed to delete existing repository" + directory);
+        		return;
+        	}
+        	 
+        }
 
-            if (created) {
-                System.out.println("Directory created successfully.");
+        boolean created = directory.mkdirs();
 
-                String filePath = directoryPath + File.separator + fileName;
+        if (created) {
+                System.out.println("Directory created successfully." + directory);
+
+                String filePath = directoryPath + File.separator + directory + "fileName";
                 File file = new File(filePath);
 
                 try {
+                	file.getParentFile().mkdirs();
                     if (file.createNewFile()) {
                         System.out.println("File created successfully.");
                     } else {
@@ -44,6 +46,26 @@ public class DirectoryHelper {
             }
         }
 
-        scanner.close();
+        private static boolean deleteDirectory(File directory) {
+        	if (directory.exists()) {
+                File[] files = directory.listFiles();
+                if (files != null) {
+                    for (File file : files) {
+                        if (file.isDirectory()) {
+                            deleteDirectory(file);
+                        } else {
+                            file.delete();
+                        }
+                    }
+                }
+            }
+            return directory.delete();        
     }
+    
 }
+
+// TODO:
+// remove the  System out
+//and remove the prompt for input
+// file.directory
+// // delete directory parent and children
