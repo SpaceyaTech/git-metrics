3ea8f125ac5458818186ce458114ac01d1254203
David <davidongora@gmail.com>
2023-07-14 12:13:52 +0300

    feat: added the propertySource annotation to help read the yaml file

--- src/main/java/com/spaceyatech/gitmetrics/GitmetricsApplication.java
+++ src/main/java/com/spaceyatech/gitmetrics/GitmetricsApplication.java
@@ -2,8 +2,11 @@ package com.spaceyatech.gitmetrics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
 
 @SpringBootApplication
+@PropertySource("classpath:config.yaml")
 public class GitmetricsApplication {
 
     public static void main( String[] args ) {
